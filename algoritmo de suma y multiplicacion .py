96d0b163d48b7c53e06b3b6a7ff91b41e19186c9
gimenavargas <1471366@senati.pe>
2023-02-23 11:32:00 -0500

    suma en python

---
+++ algoritmo de suma y multiplicacion .py
@@ -0,0 +1,12 @@
+#DESARROLLAR UN ALGORITMO DE LA SUMA Y 
+# MULTIPLICACION CON DOS NUMEROS 
+
+#suma
+n1 = 4
+n2 = 6
+print (n1+n2)
+
+#multiplicacion
+n1=3
+n2=2
+print (n1*n2)
---
+++ suma.py
@@ -0,0 +1,5 @@
+#SUMA 
+
+n1=4
+n2=3
+print(n1+n2)
